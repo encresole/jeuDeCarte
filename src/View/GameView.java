53f5d0a996aed601039f995eafbecd60d053cea6
Jonathan <encresole@gmail.com>
2026-03-26 09:53:48 +0100

    debut du panel pour le jeu

--- src/View/GameView.java
+++ src/View/GameView.java
@@ -1,6 +1,7 @@
 package View;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 
 public class GameView extends PanelAgesOfClash {
 	private static final long serialVersionUID = 1L;
@@ -18,6 +19,7 @@ public class GameView extends PanelAgesOfClash {
 		boutonRetour.setActionCommand("SHOWMENU");
 		boutonRetour.addActionListener(menuManager.menuController);
 		
+		panelJ1.setPreferredSize(new Dimension(600,400));
 		add(panelJ1, BorderLayout.LINE_START);
 		add(panelJ2, BorderLayout.LINE_END);
 		add(boutonRetour,BorderLayout.PAGE_END);
--- src/View/PanelJoueur.java
+++ src/View/PanelJoueur.java
@@ -1,6 +1,9 @@
 package View;
 
+import java.awt.BorderLayout;
+
 import javax.swing.JPanel;
+import Model.Personnage;
 
 public class PanelJoueur extends JPanel{
 	private static final long serialVersionUID = 1L;
@@ -8,6 +11,16 @@ public class PanelJoueur extends JPanel{
 	PanelAgesOfClash panelBanc = new PanelAgesOfClash();
 	PanelAgesOfClash panelMain = new PanelAgesOfClash();
 	
+	
+	public PanelJoueur() {
+		// TODO Auto-generated constructor stub
+		setLayout(new BorderLayout());
+		add(panelBanc,BorderLayout.LINE_START);
+		add(panelMain,BorderLayout.SOUTH);
+		panelBanc.add(new Personnage("","Aldric","Sire Aldric l'Indomptable","/images/personnages/Sire Aldric l'indomptable.jpg",null,150,100));
+		panelBanc.add(new Personnage("","Kenshi","Kenshi la Lame Silencieuse","/images/personnages/Kenshi la lame silencieuse.jpg",null,110,80));
+		panelBanc.add(new Personnage("","AgentFantome","Kenshi la Lame Silencieuse","/images/personnages/Agent Fantome.jpg",null,110,80));
+	}
 	
 
 }
