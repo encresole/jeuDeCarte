805b52df6e50a42441cf50f4e18670cd178ee396
Raphael <raphael.v3109@gmail.com>
2026-03-26 09:27:31 +0100

    Effet commencé et Model,correction de Toad

--- src/Model/Joueur.java
+++ src/Model/Joueur.java
@@ -3,26 +3,25 @@ package Model;
 import java.util.ArrayList;
 
 public class Joueur {
-	
-	public String name;
-	public Carte actif; 
-	public ArrayList<Carte> banc = new ArrayList<Carte>();
-	public ArrayList<Carte> main = new ArrayList<Carte>();
-	public Deck deck = new Deck();
-	
-	public Joueur(String name) {
-		// TODO Auto-generated constructor stub
-		this.name=name;
-	}
-	
-	public void ajouterActif(int i) {
-		actif=main.get(i);
-		main.remove(i);
-	}
-	
-	@Override
-	public String toString() {
-		// TODO Auto-generated method stub
-		return name;
-	}
+
+    public String name;
+    public Carte actif;
+    public ArrayList<Carte> banc = new ArrayList<Carte>();
+    public ArrayList<Carte> main = new ArrayList<Carte>();
+    public Deck deck = new Deck();
+
+    public Joueur(String name) {
+        this.name = name;
+    }
+
+    public void ajouterActif(int i) {
+        actif = main.get(i);
+        main.remove(i);
+    }
+
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
